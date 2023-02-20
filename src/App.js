f3c6943b9d21f82ceec79ae173605b61de85b208
Iyin0 <p2iyinoluwa@gmail.com>
2023-02-20 13:39:21 +0100

    portfolio web app link update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 import LandingPage from "./landingPage";
-import { Routes, Route, HashRouter } from 'react-router-dom'
+import { Outlet, createBrowserRouter, RouterProvider } from 'react-router-dom'
 import ProjectDetails from "./ProjectDetails";
 import Footer from "./Footer";
 
 function App() {
-  return (
-    <HashRouter>
+
+
+  const Layout = () => {
+    return (
       <div className="App">
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/project/:id" element={<ProjectDetails />} />
-        </Routes>
+        <Outlet />
         <Footer />
       </div>
-    </HashRouter>
-  );
+    )
+  }
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          path: '/',
+          element: <LandingPage />
+        },
+        {
+          path: '/project/:id',
+          element: <ProjectDetails />
+        }
+      ]
+    }
+  ])
+
+  return <RouterProvider router={router} />
 }
 
 export default App;
