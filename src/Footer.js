e4327f8e539526aa24a6627b8020d3cb21c6b879
Iyin0 <p2iyinoluwa@gmail.com>
2023-04-28 14:38:09 +0100

    year update

--- src/Footer.js
+++ src/Footer.js
@@ -1,6 +1,15 @@
+import { useEffect, useState } from 'react';
 import { BsGithub, BsLinkedin } from 'react-icons/bs'
 
 const Footer = () => {
+
+    const [date, setDate] = useState('')
+
+    useEffect(() => {
+        const d = new Date();
+        setDate(d.getFullYear())
+    }, [])
+
     return (
         <footer>
             <div>
@@ -8,7 +17,7 @@ const Footer = () => {
                 <p className='available'>I'm availale for full-time, part-time, freelance and contract work.</p>
                 <a href="mailto: p2iyinoluwa@gmail.com">SAY HELLO</a>
             </div>
-            <p className='copyright'><span>&#9400; Iyinoluwa Bilewu 2022</span> <span>|</span> <a href="https://github.com/Iyin0"><BsGithub /></a> <a href="https://www.linkedin.com/in/isaac-bilewu-906716196/"><BsLinkedin /></a></p>
+            <p className='copyright'><span>&#9400; Iyinoluwa Bilewu {date}</span> <span>|</span> <a href="https://github.com/Iyin0"><BsGithub /></a> <a href="https://www.linkedin.com/in/isaac-bilewu-906716196/"><BsLinkedin /></a></p>
         </footer>
     );
 }
