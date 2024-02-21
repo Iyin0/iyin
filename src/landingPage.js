66b36b1fb8a7691589218874957a7c1d448f2004
iyin0 <p2iyinoluwa@gmail.com>
2024-02-21 10:15:58 +0100

    some fixes

--- src/landingPage.js
+++ src/landingPage.js
@@ -12,30 +12,32 @@ const LandingPage = () => {
     return (
         <div className="landingPage">
             <section className="front">
-                <div className="container" data-aos="fade-right">
-                    <div className="intro">
-                        <div className="bio">
-                            <h1>Iyinoluwa Bilewu</h1>
-                            <h2>Fullstack Web Developer</h2>
-                            <p>
-                                Hey there! I'm iyin, a passionate software developer specializing in crafting dynamic
-                                mobile applications using Flutter and sleek websites with React.js and Next.js. I excel
-                                in utilizing a diverse tech stack that includes SCSS, JavaScript, TypeScript, Node.js,
-                                and Nest.js. With a keen eye for detail, I've honed my skills in developing robust
-                                servers using Node.js and Nest.js. My expertise extends to designing software
-                                architecture and managing databases, where I've adeptly worked with MySQL and MongoDB.
-                                Additionally, I'm well-versed in leveraging  cloud services like AWS for seamless deployment -
-                                from setting up servers on AWS EC2  through GitHub Actions and Docker to harnessing AWS storage
-                                solutions such as RDS for SQL  data storage and S3 for efficient file management.
-                            </p>
+                <div className="container-back">
+                    <div className="container" data-aos="fade-right">
+                        <div className="intro">
+                            <div className="bio">
+                                <h1>Iyinoluwa Bilewu</h1>
+                                <h2>Software Developer</h2>
+                                <p>
+                                    Hey there! I'm a passionate software developer specializing in crafting dynamic
+                                    mobile applications using Flutter and sleek websites with React.js and Next.js. With a keen eye for detail,
+                                    I've honed my skills in developing robust
+                                    servers using Node.js and Nest.js. My expertise extends to designing software
+                                    architecture and managing databases, where I've adeptly worked with MySQL and MongoDB.
+                                    Additionally, I'm well-versed in leveraging  cloud services like AWS for seamless deployment -
+                                    from setting up servers on AWS EC2  through GitHub Actions and Docker to harnessing AWS storage
+                                    solutions such as RDS for SQL  data storage and S3 for efficient file management.
+                                </p>
+                                <p>My stacks are Flutter, JavaScript, Typescript, React.js, Next.js, CSS, SASS, Node.js, Nest.js, MySQL, MongoDB, AWS, Docker</p>
+                            </div>
+                            <HashLink smooth to="/" className="project-link" onClick={scroll}>SEE MY PROJECTS</HashLink>
+                            <p className='all-links'><a href="https://github.com/Iyin0" className="links">Github</a> / <a href="https://www.linkedin.com/in/isaac-bilewu/" className="links">LinkedIn</a> / <a href="https://drive.google.com/file/d/1WLTdXK3IeLdsGVMfYlhUJnLtz9rdzTHU/view?usp=sharing" className="links">Resume</a></p>
                         </div>
-                        <HashLink smooth to="/" className="project-link" onClick={scroll}>SEE MY PROJECTS</HashLink>
-                        <p className='all-links'><a href="https://github.com/Iyin0" className="links">Github</a> / <a href="https://www.linkedin.com/in/isaac-bilewu/" className="links">LinkedIn</a> / <a href="https://drive.google.com/file/d/1WLTdXK3IeLdsGVMfYlhUJnLtz9rdzTHU/view?usp=sharing" className="links">Resume</a></p>
                     </div>
-                </div>
-                <div className="container" data-aos="fade-left">
-                    <div className="pics">
-                        <img src={require("./images/logo_LOGO_resized.png")} alt="logo" />
+                    <div className="container" data-aos="fade-left">
+                        <div className="pics">
+                            <img src={require("./images/logo_LOGO_resized.png")} alt="logo" />
+                        </div>
                     </div>
                 </div>
             </section>
