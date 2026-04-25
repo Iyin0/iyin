812607924d34bfafaaa1e6c90fe908ad1a7dcbde
iyin0 <p2iyinoluwa@gmail.com>
2026-04-25 15:50:58 +0100

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+if (process.env.NODE_ENV === "development") {
+  const { initOpenNextCloudflareForDev } = await import("@opennextjs/cloudflare");
+  initOpenNextCloudflareForDev();
+}
 
-import('@opennextjs/cloudflare').then(m => m.initOpenNextCloudflareForDev());
+export default nextConfig;
